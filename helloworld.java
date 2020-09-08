543604d815038c63d568f8d2ad94337b4a382f0b
khaledalmunayes <64110882+khaledalmunayes@users.noreply.github.com>
2020-09-08 04:15:54 +0300

    create new helloworld.java
    trying to see

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+        public static void main( String []args ) {
+        System.out.println( "Hello, my name is Khaled" );
+        System.out.println( "I will be your virtual assistant!" );
+        System.out.println( "Please, be my guest" );
+        System.out.println( "Here You go!!" );
+        System.out.println( "There i go!!!" );
+        //System.out.println("")
+        //this is a comment
+        System.exit(0);
+    }
+
+}
